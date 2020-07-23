be0c5ca0acc3817e864d6f752cbe76db99fdc44d
seladb <pcapplusplus@gmail.com>
2020-07-23 01:13:59 -0700

    bla11

--- ci/install_npcap.bat
+++ ci/install_npcap.bat
@@ -3,14 +3,15 @@ if defined NPCAP_USERNAME set /A NPCAP_OEM_CREDENTIALS_DEFINED=NPCAP_OEM_CREDENT
 if defined NPCAP_PASSWORD set /A NPCAP_OEM_CREDENTIALS_DEFINED=NPCAP_OEM_CREDENTIALS_DEFINED+1
 
 if "%NPCAP_OEM_CREDENTIALS_DEFINED%"=="2" (
-	set NPCAP_FILE=npcap-0.9995-oem.exe
+	set NPCAP_FILE=npcap-0.96.exe
 ) else (
 	set NPCAP_FILE=npcap-0.96.exe
 )
 
 if "%NPCAP_OEM_CREDENTIALS_DEFINED%"=="2" (
 	echo Using Npcap OEM version %NPCAP_FILE%
-	curl --digest --user %NPCAP_USERNAME%:%NPCAP_PASSWORD% https://nmap.org/npcap/oem/dist/%NPCAP_FILE% --output %NPCAP_FILE%
+	appveyor DownloadFile https://nmap.org/npcap/dist/%NPCAP_FILE%
+	rem curl --digest --user %NPCAP_USERNAME%:%NPCAP_PASSWORD% https://nmap.org/npcap/oem/dist/%NPCAP_FILE% --output %NPCAP_FILE%
 	dir
 ) else (
 	echo Using Npcap free version %NPCAP_FILE%
