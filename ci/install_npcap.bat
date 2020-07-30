dccdae7735859ca8667a0b7d44aa86c49d6e41bf
seladb <pcapplusplus@gmail.com>
2020-07-30 00:05:40 -0700

    bla53

--- ci/install_npcap.bat
+++ ci/install_npcap.bat
@@ -18,6 +18,18 @@ if "%NPCAP_OEM_CREDENTIALS_DEFINED%"=="2" (
 	appveyor DownloadFile https://nmap.org/npcap/dist/%NPCAP_FILE%
 )
 
+echo BEFORE DOWNLOAD NPCAP SDK
+
+curl https://nmap.org/npcap/dist/npcap-sdk-1.04.zip --output npcap-sdk-1.04.zip
+
+echo AFTER DOWNLOAD NPCAP SDK
+dir
+mkdir C:\Npcap-sdk
+
+echo BEFORE 7Z RUN
+7z x .\npcap-sdk-1.04.zip -oC:\Npcap-sdk
+echo AFTER 7Z RUN
+
 echo BEFORE NPCAP INSTALL
 
 %NPCAP_FILE% /S
@@ -30,15 +42,3 @@ if not "%NPCAP_OEM_CREDENTIALS_DEFINED%"=="2" (
 	xcopy C:\Windows\SysWOW64\Npcap\*.dll C:\Windows\SysWOW64
 	echo DONE CHANGES
 )
-
-echo BEFORE DOWNLOAD NPCAP SDK
-
-curl https://nmap.org/npcap/dist/npcap-sdk-1.04.zip --output npcap-sdk-1.04.zip
-
-echo AFTER DOWNLOAD NPCAP SDK
-dir
-mkdir C:\Npcap-sdk
-
-echo BEFORE 7Z RUN
-7z x .\npcap-sdk-1.04.zip -oC:\Npcap-sdk
-echo AFTER 7Z RUN
